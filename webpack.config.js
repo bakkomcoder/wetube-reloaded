160f7b8098f16265bcb12389bcc5ee40a03855e0
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-05-05 15:48:37 +0900

    Update server.js
    static 주소를 통해 asset 파일 공개

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
 const path = require("path");
 
-// console.log(__dirname);
-
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
   output: {
     filename: "main.js",
-    // path: "./assets/js",
     path: path.resolve(__dirname, "assets", "js"),
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // js 코드를
+        test: /\.js$/,
         use: {
-          loader: "babel-loader", // babel-loader라는 Loader로 가공
+          loader: "babel-loader",
           options: {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
       },
     ],
-    ㅇ,
   },
 };
