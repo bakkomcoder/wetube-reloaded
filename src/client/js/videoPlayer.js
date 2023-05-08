e9c0a00e27a90a1607f413ff7e4ddbc419ae0c59
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-05-08 22:58:21 +0900

    Update Play, Mute
    Play / Mute Button 코드 클린하게

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,27 +4,23 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 
-const handlePlayClick = (event) => {
+const handlePlayClick = () => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
 
-const handleMuteClick = (event) => {
+const handleMuteClick = () => {
   if (video.muted) {
     video.muted = false;
-    muteBtn.innerText = "Mute";
   } else {
     video.muted = true;
-    muteBtn.innerText = "Unmute";
   }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
