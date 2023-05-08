b6917772d6309fe1655515b31906758ea35a4480
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-05-08 22:46:04 +0900

    Play, Mute Button

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,30 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (event) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMuteClick = (event) => {
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  } else {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
+  }
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
