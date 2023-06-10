5db6fb4aa19b54e0cc1fa27cd33c55771b06b391
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-06-10 21:11:37 +0900

    Webassembly
    비디오 녹화본에서 썸네일 추출하여 다운로드 (두번씩 다운로드 되는 에러 있음)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,4 +1,4 @@
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg"; // transcode video
 const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
 
@@ -6,46 +6,27 @@ let stream;
 let recorder;
 let videoFile;
 
-const files = {
-  input: "recording.webm",
-  output: "output.mp4",
-  thumb: "thumbnail.jpg",
-};
-
-const downloadFile = (fileUrl, fileName) => {
-  const a = document.createElement("a");
-  a.href = fileUrl;
-  a.download = fileName;
-  document.body.appendChild(a);
-  a.click();
-};
-
 const handleDownload = async () => {
-  actionBtn.removeEventListener("click", handleDownload);
-
-  actionBtn.innerText = "변환중...";
-
-  actionBtn.disabled = true;
-
   const ffmpeg = createFFmpeg({ log: true });
-  await ffmpeg.load();
-
-  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
-
-  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
-
+  await ffmpeg.load(); // 사용자의 브라우저에서 소프트웨어 불러와서
+  // create file
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile)); // 가상의 세계에 파일을 생성. FS("writeFile", file명, 불러오는 binary 데이터)
+  // transcode file
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4"); // 초당 60프레임 mp4 형식으로 인코딩
+  // thumbnail
   await ffmpeg.run(
     "-i",
-    files.input,
+    "recording.webm",
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    files.thumb
-  );
+    "thumbnail.jpg"
+  ); // "recording.webm"을 인풋으로 받고 00:00:01 시간대를 찾고 1장의 스크린샷을 찍어서 그 파일을 아웃풋 "thumbnail.jpg"로 저장. 그 파일은 파일시스템(FS)의 메모리에 만들어진다.
 
-  const mp4File = ffmpeg.FS("readFile", files.output);
-  const thumbFile = ffmpeg.FS("readFile", files.thumb);
+  // download transcoded video & thumbFile
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
 
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
   const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
@@ -53,33 +34,39 @@ const handleDownload = async () => {
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  downloadFile(mp4Url, "MyRecording.mp4");
-  downloadFile(thumbUrl, "MyThumbnail.jpg");
+  const a = document.createElement("a");
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
+  document.body.appendChild(a);
+  a.click();
 
-  ffmpeg.FS("unlink", files.input);
-  ffmpeg.FS("unlink", files.output);
-  ffmpeg.FS("unlink", files.thumb);
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbUrl;
+  thumbA.download = "MyThumbnail.jpg";
+  document.body.appendChild(thumbA);
+  thumbA.click();
+
+  // unlink & 메모리에서 소스파일 삭제
+  ffmpeg.FS("unlink", "recording.webm");
+  ffmpeg.FS("unlink", "output.mp4");
+  ffmpeg.FS("unlink", "thumbnail.jpg");
 
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
   URL.revokeObjectURL(videoFile);
-
-  actionBtn.disabled = false;
-  actionBtn.innerText = "다시 녹화하기";
-  actionBtn.addEventListener("click", handleStart);
 };
 
 const handleStop = () => {
-  actionBtn.innerText = "녹화본 다운로드";
+  actionBtn.innerText = "Download Recording";
   actionBtn.removeEventListener("click", handleStop);
   actionBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
 
 const handleStart = () => {
-  actionBtn.innerText = "녹화 시작!";
-  actionBtn.disabled = true;
+  actionBtn.innerText = "Stop Recording";
   actionBtn.removeEventListener("click", handleStart);
+  actionBtn.addEventListener("click", handleStop);
   recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data);
@@ -87,23 +74,14 @@ const handleStart = () => {
     video.src = videoFile;
     video.loop = true;
     video.play();
-    actionBtn.innerText = "녹화본 다운로드";
-    actionBtn.disabled = false;
-    actionBtn.addEventListener("click", handleDownload);
   };
   recorder.start();
-  setTimeout(() => {
-    recorder.stop();
-  }, 5000);
 };
 
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
-    video: {
-      width: 1024,
-      height: 576,
-    },
+    video: true,
   });
   video.srcObject = stream;
   video.play();
--- src/server.js
+++ src/server.js
@@ -14,11 +14,11 @@ const logger = morgan("tiny");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-// app.use((req, res, next) => {
-//   res.header("Cross-Origin-Embedder-Policy", "require-corp");
-//   res.header("Cross-Origin-Opener-Policy", "same-origin");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // (commentSection.js) 댓글 text를 string으로 받아 JS object로 돌려줌
